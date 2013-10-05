a5a9956f4a6a750092a5756051faac6dd2f56797
Chris Jeane <chrisjeane@gmail.com>
2013-10-05 14:29:16 -0500

    Setups player with test network video stream.

--- iOS/Boxing/Boxing/Controllers/HBMainViewContoller.h
+++ iOS/Boxing/Boxing/Controllers/HBMainViewContoller.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface HBMainViewContoller : UIViewController
 
--- iOS/Boxing/Boxing/Controllers/HBMainViewContoller.m
+++ iOS/Boxing/Boxing/Controllers/HBMainViewContoller.m
@@ -7,11 +7,14 @@
 //
 
 #import "HBMainViewContoller.h"
-#import "HBPoster.h"
+#import "UIView+Autolayout.h"
 
 @interface HBMainViewContoller ()
 
-@property HBPoster *poster;
+@property UIImageView *posterImageView;
+@property MPMoviePlayerController *moviePlayer;
+@property UIView *playerView;
+@property bool isRotated;
 
 @end
 
@@ -30,29 +33,84 @@
 {
     [super viewDidLoad];
 	[self setupPoster];
+    [self setupPlayerView];
 }
 
 - (void)setupPoster
 {
-    self.poster = [[HBPoster alloc] initWithImage:[UIImage imageNamed:@"poster"]];
-    [self.view addSubview:self.poster];
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.poster
+    self.posterImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"poster"]];
+    self.posterImageView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.posterImageView.userInteractionEnabled = YES;
+    [self.view addSubview:self.posterImageView];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.posterImageView
                                                           attribute:NSLayoutAttributeCenterX
                                                           relatedBy:NSLayoutRelationEqual
-                                                             toItem:self.poster.superview
+                                                             toItem:self.view
                                                           attribute:NSLayoutAttributeCenterX
                                                          multiplier:1.f
                                                            constant:0.f]];
     
-    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.poster
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.posterImageView
                                                           attribute:NSLayoutAttributeCenterY
                                                           relatedBy:NSLayoutRelationEqual
-                                                             toItem:self.poster.superview
+                                                             toItem:self.view
                                                           attribute:NSLayoutAttributeCenterY
                                                          multiplier:1.f
                                                            constant:0.f]];
-    [self.poster addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self.poster
-                                                                              action:@selector(rotate)]];
+    
+    [self.posterImageView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                       action:@selector(rotate)]];
+    
+}
+
+- (void)setupPlayerView
+{
+    self.playerView = [[UIView alloc] init];
+    self.playerView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.playerView.backgroundColor = [UIColor blackColor];
+    
+    [self.posterImageView addSubview:self.playerView];
+    [self.posterImageView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-[_playerView]-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(_playerView)]];
+    [self.posterImageView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[_playerView]-|"
+                                                                      options:0
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(_playerView)]];
+    self.playerView.hidden = YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    NSURL *contentUrl = [NSURL URLWithString:@"http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"];
+    self.moviePlayer = [[MPMoviePlayerController alloc] initWithContentURL:contentUrl];
+    [self.moviePlayer prepareToPlay];
+    self.moviePlayer.controlStyle = MPMovieControlStyleEmbedded;
+    [self.moviePlayer.view setFrame:self.playerView.bounds];
+    [self.playerView addSubview:self.moviePlayer.view];
+}
+
+- (void)rotate
+{
+    self.isRotated = !self.isRotated;
+    
+    [UIView beginAnimations:@"PosterRotate" context:nil];
+    
+    [UIView setAnimationDuration:0.5f];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
+    [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.posterImageView cache:YES];
+    
+    if (self.isRotated) {
+        self.playerView.hidden = NO;
+        [self.moviePlayer play];
+    } else {
+        self.playerView.hidden = YES;
+        [self.moviePlayer pause];
+    }
+    
+    [UIView commitAnimations];
+    
 }
 
 - (void)didReceiveMemoryWarning
